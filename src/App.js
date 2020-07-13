b7f3a6ccb595fce3696b305053f27cbd17461105
DavidNakov <nakov_david@mail.bg>
2020-07-13 15:11:35 +0300

    working on login form
    still uncomplete;
    TO DO: fb button finish and clear padd and margin;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header.js';
+import LoginForm from './Components/LoginForm/LoginForm.js';
 
 function App() {
   return (
-    <Header visible={false}/>
+    <div className="app_container">
+      <LoginForm />
+    </div>
   );
 }
 
