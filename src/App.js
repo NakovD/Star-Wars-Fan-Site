ca1d5af9ba3cb22f8de607f25c37e95777151d15
DavidNakov <nakov_david@mail.bg>
2020-07-15 11:28:43 +0300

    typos fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HeroCard from './Components/HeroCard/HeroCard.js';
 function App() {
   return (
     <div className="app_container">
-      
+      {/* <HeroCard img="https://www.da-files.com/artnetwork/zeitgeist/starwars-fan-art/63-img-23.jpg"/> */}
     </div>
   );
 }
--- src/Components/HeroCard/HeroCard.js
+++ src/Components/HeroCard/HeroCard.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import './HeroCard.css';
+import styles from './HeroCard.module.css';
 
 
 const HeroCard = ({ img }) => {
 
 
     return (
-        <div className="wrapper">
-            <div className="card">
+        <div className={styles.wrapper}>
+            <div className={styles.card}>
                 <img src={img}></img>
-                <div className="descriptions">
+                <div className={styles.descriptions}>
                     <h1>Anakin Skywalker</h1>
                     <button>
-                        <i className="fab fa-youtube"></i>
                     Click for more details
                 </button>
                 </div>
